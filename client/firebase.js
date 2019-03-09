f2fbd6a96c85e13147e6f01106ee84fc921ea205
Rails Udemy Course <prete316@gmail.com>
2019-03-09 16:49:39 -0600

    update firebase security

--- client/firebase.js
+++ client/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/storage'
 
 
 export const DB_CONFIG = {
-  apiKey: "AIzaSyCbtc5iyQhog6UcnTu-dhoqcqP1jtCc6II",
-  authDomain: "tso-file-storage.firebaseapp.com",
-  databaseURL: "https://tso-file-storage.firebaseio.com",
-  projectId: "tso-file-storage",
-  storageBucket: "tso-file-storage.appspot.com",
-  messagingSenderId: "501536158639"
+
 };
 
 firebase.initializeApp(DB_CONFIG);
